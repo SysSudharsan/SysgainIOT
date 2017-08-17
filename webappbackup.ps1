f20e15fcdc640784c83d7e6c0d0f1368ac2c3bcb
SysSudharsan <SysSudharsan@users.noreply.github.com>
2017-08-17 20:32:44 +0530

    Update webappbackup.ps1

--- webappbackup.ps1
+++ webappbackup.ps1
@@ -18,7 +18,7 @@ $storageAccountName = "webbackupacc"
     $storageAccountKey = Get-AzureRmStorageAccountKey -ResourceGroupName $storageAccountRg -Name $storageAccountName
     $context = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey[0].Value
 
-    $blobContainerName = "backup5"
+    $blobContainerName = "backup6"
     $sasUrl = New-AzureStorageContainerSASToken -Name $blobContainerName -Permission rwdl -Context $context -ExpiryTime (Get-Date).AddMonths(1) -FullUri
     $url = Write-Output $sasUrl
 
@@ -32,3 +32,8 @@ $sasUrl1 = $url
 
     $dbSetting1 = New-AzureRmWebAppDatabaseBackupSetting -Name webappDB -DatabaseType SqlAzure -ConnectionString "Server=tcp:weappsudhar.database.windows.net,1433;Initial Catalog=webappDB;Persist Security Info=False;User ID=dbadmin;Password=Welcome12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=50;"
     $dbBackup = New-AzureRmWebAppBackup -ResourceGroupName $resourceGroupName -Name $appName -BackupName SudharBackup -StorageAccountUrl $sasUrl1 -Databases $dbSetting1
+
+
+    Edit-AzureRmWebAppBackupConfiguration -Name $appName -ResourceGroupName $resourceGroupName `
+      -StorageAccountUrl $sasUrl1 -FrequencyInterval 6 -FrequencyUnit Hour -Databases $dbSetting1 `
+      -KeepAtLeastOneBackup -StartTime (Get-Date).AddHours(1)

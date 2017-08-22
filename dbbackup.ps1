c380adb82e26a425ac2a07558acf5c35961e649d
SysSudharsan <SysSudharsan@users.noreply.github.com>
2017-08-22 17:36:58 +0530

    Update dbbackup.ps1

--- dbbackup.ps1
+++ dbbackup.ps1
@@ -1,47 +1,92 @@
 <#
 Long-term backup retention is currently in preview and available in the following regions: 
-Australia East, Australia Southeast, Brazil South, 
-Central US, East Asia, East US, East US 2, India Central, India South, Japan East, 
-Japan West, North Central US, North Europe, South Central US, Southeast Asia, West Europe, and West US.
+Australia East, Australia Southeast, Brazil South, Central US, East Asia, East US, East US 2, 
+India Central, India South, Japan East, Japan West, North Central US, North Europe, South Central US, 
+Southeast Asia, West Europe, and West US.
 #>
 
-$azureAccountName ="nvtuluva@sysgaincloud.onmicrosoft.com"
-$azurePassword = ConvertTo-SecureString "indiatimes@225" -AsPlainText -Force
-$psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
-Login-AzureRmAccount -Credential $psCred
-Get-AzureRmSubscription
+workflow dbbackupscript{
+param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $azureAccountName,
 
-# Create a recovery services vault
+        [Parameter(Mandatory=$true)]
+        [string]
+        $azurePassword,
 
-$resourceGroupName = "srikala-iot5"
-$serverName = "trendsqlerxwa"
+        [Parameter(Mandatory=$true)]
+        [string]
+        $resourceGroupName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $sqlserverName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $recoveryServiceVaultName,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $RetentionDurationType,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $RetentionCount
+
+) 
+
+InlineScript{
+
+        $azureAccountName = $Using:azureAccountName
+        $azurePassword = $Using:azurePassword
+        $resourceGroupName = $Using:resourceGroupName
+        $sqlserverName = $Using:sqlserverName
+        $recoveryServiceVaultName = $Using:recoveryServiceVaultName
+        $RetentionDurationType = $Using:RetentionDurationType
+        $RetentionCount = $Using:RetentionCount
+
+ # $azureAccountName ="nvtuluva@sysgaincloud.onmicrosoft.com"
+ # $azurePassword = ConvertTo-SecureString "indiatimes@225" -AsPlainText -Force
+ $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
+ Login-AzureRmAccount -Credential $psCred
+ Get-AzureRmSubscription
+
+ # Create a recovery services vault
+
+ # $resourceGroupName = "srikala-iot5"
+ # $serverName = "trendsqlerxwa"
 
 
-$serverLocation = (Get-AzureRmSqlServer -ServerName $serverName -ResourceGroupName $resourceGroupName).Location
-$recoveryServiceVaultName = "vmbackupvault567"
+ $serverLocation = (Get-AzureRmSqlServer -ServerName $sqlserverName -ResourceGroupName $resourceGroupName).Location
+ # $recoveryServiceVaultName = "vmbackupvault567"
 
-$vault = New-AzureRmRecoveryServicesVault -Name $recoveryServiceVaultName -ResourceGroupName $ResourceGroupName -Location $serverLocation 
-Set-AzureRmRecoveryServicesBackupProperties -BackupStorageRedundancy LocallyRedundant -Vault $vault
+ $vault = New-AzureRmRecoveryServicesVault -Name $recoveryServiceVaultName -ResourceGroupName $resourceGroupName -Location $serverLocation 
+ Set-AzureRmRecoveryServicesBackupProperties -BackupStorageRedundancy LocallyRedundant -Vault $vault
 
-# Set your server to use the vault to for long-term backup retention 
+ # Set your server to use the vault to for long-term backup retention 
 
-Set-AzureRmSqlServerBackupLongTermRetentionVault -ResourceGroupName $resourceGroupName -ServerName $serverName -ResourceId $vault.Id
+ Set-AzureRmSqlServerBackupLongTermRetentionVault -ResourceGroupName $resourceGroupName -ServerName $serverName -ResourceId $vault.Id
 
-# Retrieve the default retention policy for the AzureSQLDatabase workload type
-$retentionPolicy = Get-AzureRmRecoveryServicesBackupRetentionPolicyObject -WorkloadType AzureSQLDatabase
+ # Retrieve the default retention policy for the AzureSQLDatabase workload type
+ $retentionPolicy = Get-AzureRmRecoveryServicesBackupRetentionPolicyObject -WorkloadType AzureSQLDatabase
 
-# Set the retention value to two years (you can set to any time between 1 week and 10 years)
-$retentionPolicy.RetentionDurationType = "Weeks"
-$retentionPolicy.RetentionCount = 4
-$retentionPolicyName = "my1MonthRetentionPolicy"
+ # Set the retention value to two years (you can set to any time between 1 week and 10 years)
+ $retentionPolicy.RetentionDurationType = $RetentionDurationType
+ $retentionPolicy.RetentionCount = $RetentionCount
+ $retentionPolicyName = "dbRetentionPolicy"
 
-# Set the vault context to the vault you are creating the policy for
-Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+ # Set the vault context to the vault you are creating the policy for
+ Set-AzureRmRecoveryServicesVaultContext -Vault $vault
 
-# Create the new policy
-$policy = New-AzureRmRecoveryServicesBackupProtectionPolicy -name $retentionPolicyName -WorkloadType AzureSQLDatabase -retentionPolicy $retentionPolicy
-$policy
+ # Create the new policy
+ $policy = New-AzureRmRecoveryServicesBackupProtectionPolicy -name $retentionPolicyName -WorkloadType AzureSQLDatabase -retentionPolicy $retentionPolicy
+ $policy
 
-# Enable long-term retention for a specific SQL database
-$policyState = "enabled"
-Set-AzureRmSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName -State $policyState -ResourceId $policy.Id
+ # Enable long-term retention for a specific SQL database
+ $policyState = "enabled"
+ Set-AzureRmSqlDatabaseBackupLongTermRetentionPolicy -ResourceGroupName $resourceGroupName -ServerName $serverName -DatabaseName $databaseName -State $policyState -ResourceId $policy.Id
+
+}
+}
